9eac608a024769a6c82b0933eb1562a116059944
Paajanen Santeri <paajanensanteri@gmail.com>
2022-02-28 16:54:28 +0200

    example configs match
    Former-commit-id: 150671abf17edfe3800416a6b2eb22a74fb905e8

--- example/initialize/config.py
+++ example/initialize/config.py
@@ -79,6 +79,20 @@ def function_val(positions):
     return np.minimum(dist1, dist2)
 
 
+############################## Unwrapping options ##############################
+
+# whether to make molecules whole from being broken over pbc
+unwrap_mols = True
+# The selection to make whole
+unwrap_sel = "protein"
+# A selection of atoms to use as "starters" for unwrapping. Unless mols_in_box=True,
+# these atoms are guaranteed to end up inside the box. Makes no difference to outcome
+# if mols_in_box=True. None means to use the smallest indexed atom of each mol.
+unwrap_starters = None
+# Put the molecule COM back into the box (ignored if not unwrapping).
+# Only considers atoms in unwrap_sel
+mols_in_box = False
+
 
 
 
@@ -90,10 +104,10 @@ maxwarn_add=True
 # At least this much data in each bin of the histogram
 data_per_bin = 100
 # Maximum amount of bins between boundaries
-maxbins = 100
+maxbins = 10
 
 # Cluster histogram
-clust_data_per_bin=1000
+clust_data_per_bin=100
 clust_maxbins=10
 
 #Max number of clusters per bin

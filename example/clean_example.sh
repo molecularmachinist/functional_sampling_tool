1a34988f3f9717b0ecdd2aed869f8a0180079ad9
Santeri Paajanen <paajanensanteri@gmail.com>
2022-06-23 14:45:33 +0300

    Added the .lock files to gitignore and cleanup
    Former-commit-id: 620fe5b1732342849976ab257cace7e2d90bf6d4

--- example/clean_example.sh
+++ example/clean_example.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf initialize/epoch01 after_e1/epoch02 after_e1/figs
-rm -rf after_e1/epoch01/rep*/fval_data.npz  after_e1/epoch01/rep*/.mdrun.xtc_offsets.npz
+rm -rf after_e1/epoch01/rep*/fval_data.npz  after_e1/epoch01/rep*/.mdrun.xtc_offsets.npz after_e1/epoch01/rep*/.mdrun.xtc_offsets.lock
 rm -f after_e1/initial/start.pdb after_e1/initial/output_trjconv.txt

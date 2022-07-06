d8e1b82da61b6d0838bd9c6bdffe6d74aeeee5ec
Santeri Paajanen <paajanensanteri@gmail.com>
2022-07-06 15:49:07 +0300

    Changed trasnformations to cython accelerated
    Numba no longer is needed to run the code, but it does need to be compiled.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Extension
+import numpy
+from Cython.Build    import cythonize
+
+
+extension = Extension("functional_sampling_tool._ctransformations",
+                      sources=["lib/_ctransformations.pyx"],
+                      include_dirs=[numpy.get_include()],
+                      extra_compile_args=["-O3"],
+                      language="c++",
+                      define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')])
+
+
+setup(
+    packages    = ["functional_sampling_tool"],
+    package_dir = {"functional_sampling_tool": "src"},
+    ext_modules = cythonize(extension, language_level = "3")
+)
--- src/transformations.py
+++ src/transformations.py
@@ -3,169 +3,18 @@
 import numpy as np
 from MDAnalysis.analysis import align
 import warnings
-from numba import njit
+
+from . import _ctransformations
 
 """
 This module includes functions to make on the fly transformations for MDAnalysis trajectories
 """
 
-
-def find_frags(ag):
-    """ Traverse all molecules that atoms in ag are part of.
-        Parameters:
-            ag:       Atom group of atoms to check
-            starters: List of atoms or atom group to use as starting points. (optional)
-        Returns:
-            List of indices for each molecule, only including indices in ag.
-    """
-    # A dict to map the indices between system and selection
-    agi = set(ag.indices)
-    mols = []
-
-    # We iterate until there are no more atoms to search
-    while(agi):
-        # A set to hold info on whether we have "visited" atom
-        done = set()
-        start_i = agi.pop()
-        start = ag.universe.atoms[start_i]
-
-        # Make a stack to hold atoms to visit and add start atom
-        stack = [start]
-        mol = [start.index]
-        done.add(start.index)
-
-        # Continue until stack is empty
-        while(stack):
-            # Pop from top of stack
-            curr = stack.pop()
-            # Iterate atoms bonded to popped atom
-            for a in curr.bonded_atoms:
-                # Ignore if atom visited or if it is not in selection
-                if(a.index in done):
-                    continue
-
-                # Add atom to list
-                if(a.index in agi):
-                    mol.append(a.index)
-                    agi.remove(a.index)
-                # Add atom on top of stack and mark it as visited
-                stack.append(a)
-                done.add(a.index)
-
-
-        mols.append(np.unique(mol))
-
-    return mols
-
-
-def traverse_mol(ag, starters=[]):
-    """ Traverse all graphs of bonded atoms in ag, using atoms in starters (or the
-        smallest indexes) as starting points for a DFS.
-        Parameters:
-            ag:       Atom group of atoms to check
-            starters: List of atoms or atom group to use as starting points. (optional)
-        Returns:
-            Indexes for each bonds of the bonded atoms within the selection
-            (i.e. indexes within selection, not within whole universe). int[n,2].
-    """
-    # make sure this is a list, not ag and flip it to pop from the "start"
-    starters=list(starters)[::-1]
-    # A dict to map the indices between system and selection
-    agi_indexes = {}
-    for i,a in enumerate(ag):
-        agi_indexes[a.index] = i
-
-    # A list to hold the bonds
-    bonds = []
-
-    # We iterate until there are no more atoms to search
-    while(agi_indexes):
-        # A set to hold info on whether we have "visited" atom
-        done = set()
-        # Decide next starting atom
-        start = None
-        # Go thorugh starter atoms until it is empty or we choose one
-        while(start is None and starters):
-            a = starters.pop()
-            if a.index in agi_indexes:
-                start = a
-
-        # If we didn't choose one, taek whichever has smallest index
-        if(start is None):
-            start_i = min(agi_indexes.keys())
-            start = ag.universe.atoms[start_i]
-
-        # Make a stack to hold atoms to visit and add start atom
-        stack = [start]
-        done.add(start.index)
-        # Continue until stack is empty
-        while(stack):
-            # Pop from top of stack
-            curr = stack.pop()
-            # Iterate atoms bonded to popped atom
-            for a in curr.bonded_atoms:
-                # Ignore if atom visited or if it is not in selection
-                if(a.index in done or not a.index in agi_indexes):
-                    continue
-
-                # Add bond to list
-                bonds.append((agi_indexes[curr.index], agi_indexes[a.index]))
-                # Add atom on top of stack and mark it as visited
-                stack.append(a)
-                done.add(a.index)
-
-        # Remove visited atoms from selction to keep track of whether we are done
-        for a in done:
-            agi_indexes.pop(a)
-
-    if(not bonds):
-        warnings.warn("No bonds found in unwrap selection")
-        return np.zeros((0,2),dtype=int)
-    return np.array(bonds)
-
-
-@njit(["f8[:,:](f8[:,:],i8[:,:])",
-      "f4[:,:](f4[:,:],i8[:,:])"])
-def _iterate_bonds(pos, bonds):
-    """
-    Fixes molecules whole over pbc.
-    Parameters:
-        pos:   Positions in reciprocal space float[n,d].
-        bonds: array of atom indices corresponding to bonds.
-               The second one will be fixed. int[m,2].
-    Returns:
-        Fixed positions in reciprocal space. float[n,d].
-    """
-    # Iterate over bonds
-    for atoms in bonds:
-        a1 = atoms[0]
-        a2 = atoms[1]
-        # Difference vector
-        diff_v = pos[a2]-pos[a1]
-        # If distance is more than half box vector, translate by box vector (1.0)
-        pos[a2] -= (np.abs(diff_v)>0.5)*np.sign(diff_v)
-    # Return fixed coordinates
-    return pos
-
-
 def make_whole(ts, bonds, sel):
-    """ Makes molecules in sel whole using bond info from bonds.
-        Coordinates are first switched to reciprocal space, then passed to
-        _iterate_bonds to actually make them whole. The fixed positions are
-        switched back to normal space and set as the current positions for sel.
-    """
     box = ts.triclinic_dimensions
-    # Inverse box
-    invbox = np.linalg.inv(box)
-    # Transfer coordinates to unit cell and put in box
-    unitpos = (ts.positions[sel] @ invbox) % 1
-    # Fix box in reciprocal space
-    unitpos = _iterate_bonds(unitpos, bonds)
-
-    ts.positions[sel] = unitpos @ box
+    ts.positions[sel] = _ctransformations.make_whole(ts.positions[sel], bonds,box)
 
     return ts
-
 
 class Unwrapper:
     """ Make molecules in ag whole over the pbc. only considers
@@ -194,9 +43,11 @@ class Unwrapper:
 
     def _setup(self):
         if(self.__setup_run):
-            warnings.warn("Unwrapper setup being run after __setup_run is already." \
+            warnings.warn("Unwrapper setup being run after __setup_run is already. " \
                           "Continuing without new setup.", RuntimeWarning)
-        self.bonds = traverse_mol(self.ag, self.starters)
+        self.bonds = _ctransformations.traverse_mol(self.ag.indices,
+                                                    self.ag.universe.bonds.to_indices(),
+                                                    np.array([s.index for s in self.starters],dtype=int))
         self.__setup_run = True
         # No need to remember atom group or starters after setup
         del self.ag
@@ -232,13 +83,12 @@ class MolWrapper:
         if(self.__setup_run):
             warnings.warn("Unwrapper setup being run after __setup_run is already True." \
                           "Continuing without new setup.", RuntimeWarning)
-        self.mols = []
-        self.weights = []
-        for frag in find_frags(self.ag):
-            self.mols.append(frag)
-            self.weights.append(self.ag.universe.atoms[frag].masses)
-
-        self.totw = np.array([np.sum(w) for w in self.weights])
+        
+        self.mols, self.nmols = _ctransformations.find_frags(
+                                        self.selection,
+                                        self.ag.universe.bonds.to_indices()
+                                    )
+        self.weights = self.ag.masses.astype(self.ag.positions.dtype)
         self.__setup_run = True
         # No need to remember atom group after setup
         del self.ag
@@ -248,15 +98,13 @@ class MolWrapper:
             self._setup()
 
         box = ts.triclinic_dimensions
-        # Inverse box
-        invbox = np.linalg.inv(box)
-        for i,m in enumerate(self.mols):
-            pos = np.sum(self.weights[i]*ts.positions[m].T,axis=-1)/self.totw[i]
-            # Transfer coordinates to unit cell and put in box
-            unitpos = (pos @ invbox) % 1
-            newpos = unitpos @ box
-            trans = newpos-pos
-            ts.positions[m] += trans
+        ts.positions[self.selection] = _ctransformations.wrap_mols(
+                                                ts.positions[self.selection],
+                                                self.weights,
+                                                self.mols,
+                                                self.nmols,
+                                                box
+                                            )
         return ts
 
 
